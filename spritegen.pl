80ef678cae94bd7e8aed78bfcc80a11b0ff57ce8
Jeremy Burns <j@jaburns.net>
2015-01-24 15:40:18 -0700

    Update tiles.json path to tiles.png

--- spritegen.pl
+++ spritegen.pl
@@ -12,7 +12,7 @@ my $rows        =   8; # How many rows of blocks are there?
 my $tile_width  =  16; # How wide are the tiles?
 my $tile_height =  16;
 my $filepath    = 'public/assets/tiles.png';
-my $outfile     = 'public/assets/tiles.json';
+my $outfile     = 'tiles.json';
 
 my $height = $rows * $tile_height;
 my $index = 1;
