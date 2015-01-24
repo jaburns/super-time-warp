9b8afc83d4cd31a0e5dfb13e9638cd51dcb46216
Jeremy Burns <j@jaburns.net>
2015-01-23 22:02:03 -0700

    removed specific id property on Client. using socket.id

--- index.js
+++ index.js
@@ -25,23 +25,22 @@ var clients = [];
 
 function Client(socket) {
     this.socket = socket;
-    this.id = socket.id;
-    game.addPlayer(this.id);
+    game.addPlayer(this.socket.id);
 
     socket.on('msg input', this.receiveInput.bind(this));
     socket.emit('msg state', game.state.getState());
 
-    console.log('Client connected with ID: ' + this.id);
+    console.log('Client connected with ID: ' + this.socket.id);
 }
 
 Client.prototype.receiveInput = function(msgInput) {
-    console.log('Received input "' + msgInput + '" from client ' + this.id);
-    game.handleInput(this.id, msgInput);
+    console.log('Received input "' + msgInput + '" from client ' + this.socket.id);
+    game.handleInput(this.socket.id, msgInput);
 };
 
 Client.prototype.dispose = function() {
-    console.log('Client disconnected with ID: ' + this.id);
-    game.removePlayer(this.id);
+    console.log('Client disconnected with ID: ' + this.socket.id);
+    game.removePlayer(this.socket.id);
 };
 
 io.on('connection', function(socket) {
