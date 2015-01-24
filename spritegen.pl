338668acdd01abf5e810d441219cbcb4b945e5a3
Jeremy Burns <j@jaburns.net>
2015-01-24 15:41:13 -0700

    Fixed path to tiles

--- spritegen.pl
+++ spritegen.pl
@@ -11,8 +11,8 @@ my $width       = 256; # How many pixels wide is the sheet?
 my $rows        =   8; # How many rows of blocks are there?
 my $tile_width  =  16; # How wide are the tiles?
 my $tile_height =  16;
-my $filepath    = 'public/assets/tiles.png';
-my $outfile     = 'tiles.json';
+my $filepath    = 'tiles.png';
+my $outfile     = 'public/assets/tiles.json';
 
 my $height = $rows * $tile_height;
 my $index = 1;
