28c400e1be26ddc711e6ccf95b99c408f09b061b
Trevon <trevon.romanuik@gmail.com>
2015-01-24 12:36:50 -0700

    fixed aiming

--- game_objects/player.js
+++ game_objects/player.js
@@ -67,7 +67,7 @@ _.extend(
                     bullet.x = this.x;
                     bullet.y = this.y - (this.h / 2);
 
-                    bullet.angle = Math.atan2((this.y - this._mousePos.y), (this.x - this._mousePos.x));
+                    bullet.angle = Math.atan2((this.y - this._mousePos.y - (this.h / 2) - (bullet.h / 2)), (this.x - this._mousePos.x));
                     bullet.vx = -Math.cos(bullet.angle) * 10;
                     bullet.vy = -Math.sin(bullet.angle) * 10;
 
