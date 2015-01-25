dad3c06b0c0e79c79abc890e307c0572c361e6b7
Jordan Ching <ching.jordan@gmail.com>
2015-01-25 14:21:30 -0700

    Fixed stomp animation bug

--- public/player.js
+++ public/player.js
@@ -14,13 +14,14 @@ var playerAnimations = [
         "idle" : ['player_1', 'player_2', 'player_3', 'player_4', 'player_5', 'player_6', 'player_7', 'player_8', 'player_9', 'player_10', 'player_11'],
         "walk" : ['player_13', 'player_14', 'player_15', 'player_16', 'player_17', 'player_18'],
         "jump" : ['player_20'],
-        "fall" : ['player_21']
+        "fall" : ['player_21'],
+        "stomp": ['player_21']
     },
     {
         "idle" : ['player_81', 'player_82', 'player_83', 'player_84', 'player_85', 'player_86', 'player_87', 'player_88', 'player_89', 'player_90', 'player_91'],
         "walk" : ['player_93', 'player_94', 'player_95', 'player_96', 'player_97', 'player_98'],
-        "jump" : ['player_100'],
+        "jump" : ['player_111', 'player_112', 'player_113', 'player_114', 'player_115', 'player_116', 'player_117', 'player_118', 'player_119', 'player_120'],
         "fall" : ['player_101'],
-        "jetpack" : ['player_111', 'player_112', 'player_113', 'player_114', 'player_115', 'player_116', 'player_117', 'player_118', 'player_119', 'player_120']
+        "stomp": ['player_101']
     }
 ];
